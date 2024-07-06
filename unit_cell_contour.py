0eecf5c27e64fb667b800c15be2601db18c75810
Chettanat Padungatthakij <31538380+Chettanat-Pad@users.noreply.github.com>
2024-07-06 21:01:34 +0700

    Previous 'contour' --> 'contour_simplified'
    Previous 'unit_cell_contour' --> 'unit_cell_contour_simplified'
    New 'unit_cell_contour' calculates every film layers.

--- unit_cell_contour.py
+++ unit_cell_contour.py
@@ -1,10 +1,11 @@
 ##Function for calculating equivalent index(n), phase thickness(q)
 ##  and create an endpoint for trilayer unit cells of N size
+##  In this version, each contours in the trilayer U are calculated.
 import numpy as np
 from contour import contour
 
 def unit_cell_contour(ns, n1, n2, N, wl, npts):   #n1=outer film index, n2=cetral film index
-    #print('--unit cell contour')   #debug
+ #print('--unit cell contour')   #debug
     cr=[np.real(ns)]*(N+1)  #c[0] contains the ns of previous film layer
     #print('cr=',cr)    #debug
     ci=[np.imag(ns)]*(N+1)
@@ -12,16 +13,20 @@ def unit_cell_contour(ns, n1, n2, N, wl, npts):   #n1=outer film index, n2=cetra
     t=[None]*(N+1)
     qr=[None]*(N+1)
     qi=[None]*(N+1)
-    nf = +1j * n1
-    q2 = np.arccos(complex((-1/2)*((n1/n2)+(n2/n1)))) / (np.pi/2)
     i = 1
-    while i <= N:
+    
+    while i <= N: #The unit cell structure is H/2, L, H/2
         #print('loop ', i)  #debug
-        (cr[i],ci[i],t[i],qr[i],qi[i]) = contour(ns, nf, q2, wl, npts)
+        (cr[i],ci[i],t[i],qr[i],qi[i]) = contour(ns, n1, 0.5, wl, npts)
+        ns = cr[i][-1] + 1j*ci[i][-1]
+        (cr[i],ci[i],t[i],qr[i],qi[i]) = contour(ns, n2, 1.0, wl, npts)
+        ns = cr[i][-1] + 1j*ci[i][-1]
+        (cr[i],ci[i],t[i],qr[i],qi[i]) = contour(ns, n1, 0.5, wl, npts)
         ns = cr[i][-1] + 1j*ci[i][-1]
         cr[i]=cr[i][-1]
         ci[i]=ci[i][-1]
         i += 1
     return cr, ci, t, qr, qi
-#Both cr, ci are a array with c[0] as ns
-#c[i] as endpoint of each unit cells
+
+#c[0] is ns
+#c[i] is endpoint of each unit cells
