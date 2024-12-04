Correct the D2 calculation

D1 Fig.9a VO2 Resonant cavity structure designed for 1550nm resonant.py
@@ -61,18 +61,18 @@ nhVO2 = np.interp(wavelengths, np.asarray(r3h[0])*1000.0, np.asarray(r3h[1]-1j*n
 nAir = np.interp(wavelengths, np.asarray(r4[0])*1000.0, np.asarray(r4[1]-1j*np.asarray(r4[2])))
 
 ns = nAir	# na=1.0; Mathar 2007: n 1.3–2.5 µm
-nfc = np.concatenate((nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2,\
+nfc = np.concatenate((nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2,\
 	ncVO2,nTiO2,ncVO2,\
-	nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2)) # Join a sequence of arrays along an existing axis
-nfh = np.concatenate((nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2,\
+	nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2)) # Join a sequence of arrays along an existing axis
+nfh = np.concatenate((nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2,\
 	nhVO2,nTiO2,nhVO2,\
-	nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2)) # Join a sequence of arrays along an existing axis
+	nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2,nSiO2,nTiO2, nTiO2)) # Join a sequence of arrays along an existing axis
 wl = 1550.0	#reference wavelength
 tTiO2 = wl/(4*2.2899) # Quarterwave thick
 tSiO2 = wl/(4*1.4657) # Quarterwave thick
-thk = np.array([tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2,\
+thk = np.array([tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2,\
 	4.92,155.39,4.92,\
-	tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2])	# Thickness of each layer (in nm)
+	tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2,tSiO2,tTiO2/2, tTiO2/2])	# Thickness of each layer (in nm)
 
 ts = 500	# Substrate thickness - not relevant unless back=1
 na = 1.0	# Incident medium (ie. air) index
@@ -102,7 +102,7 @@ plt.plot(wavelengths, Ah, color="green", label="hot VO2", linestyle="dashed")
 # ax2.plot(wavelengths, Tc, color="green", label="Transmission")
 # ax2.plot(wavelengths, Th, color="green", linestyle="dashed")
 
-plt.title("Resonant cavity structure with N = 3 and M = 2 designed for\n"
+plt.title("Resonant cavity structure with N = 4 and M = 3 designed for\n"
 	"resonance at 1550 nm. Comparison between cold and hot VO2 state.", fontsize=12)
 plt.xlabel('Wavelength (nm)')
 plt.ylabel('Absorption')